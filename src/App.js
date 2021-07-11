f56a5f695123f2ee7c13f1f53ee6818481543103
Abhishek Kumar <81435274+eyemabhishek@users.noreply.github.com>
2021-07-11 14:58:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Rollingdcie from './Rollingdice'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<Rollingdcie/>
+    </div>
+  );
+}
+
+export default App;
